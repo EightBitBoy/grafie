59b8441484c029c0b51770295351b6c6cd78d2d0
Stefan Härtel <haertel.dev@gmail.com>
2017-06-23 19:23:32 +0200

    Save progress

--- src/main/groovy/de/eightbitboy/grafie/Grafie.groovy
+++ src/main/groovy/de/eightbitboy/grafie/Grafie.groovy
@@ -3,6 +3,14 @@ package de.eightbitboy.grafie
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+/*
+https://docs.gradle.org/3.3/userguide/custom_plugins.html
+https://guides.gradle.org/writing-gradle-plugins/
+https://guides.gradle.org/implementing-gradle-plugins/
+https://github.com/gradle/gradle/tree/master/subprojects
+https://speakerdeck.com/bmuschko/gradle-plugin-best-practices-by-example
+ */
+
 class Grafie implements Plugin<Project> {
     @Override
     void apply(Project project) {
