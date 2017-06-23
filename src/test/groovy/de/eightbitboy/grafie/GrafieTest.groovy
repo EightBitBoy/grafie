f6f5d991b70d9415829b6620444b1ce9d15dd188
Stefan Härtel <haertel.dev@gmail.com>
2017-06-23 08:51:39 +0200

    Use spock, implement simple test

--- src/test/groovy/de/eightbitboy/grafie/GrafieTest.groovy
+++ src/test/groovy/de/eightbitboy/grafie/GrafieTest.groovy
@@ -1,6 +1,16 @@
 package de.eightbitboy.grafie
 
-class GrafieTest extends GroovyTestCase {
-    void testApply() {
+import spock.lang.Specification
+
+class GrafieTest extends Specification {
+    def "run a simple test"() {
+        setup:
+        def numbers = [1, 2]
+
+        when:
+        numbers.add(3)
+
+        then:
+        numbers.size() == 3
     }
 }
