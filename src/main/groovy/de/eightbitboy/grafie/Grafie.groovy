a6405f8a4ab11aa84f2fee13f6c49b45f2ed8544
Stefan Härtel <haertel.dev@gmail.com>
2017-06-23 18:28:08 +0200

    Add tasks and extension object

--- src/main/groovy/de/eightbitboy/grafie/Grafie.groovy
+++ src/main/groovy/de/eightbitboy/grafie/Grafie.groovy
@@ -3,9 +3,22 @@ package de.eightbitboy.grafie
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-class Grafie implements Plugin<Project>{
+class Grafie implements Plugin<Project> {
     @Override
     void apply(Project project) {
+        project.extensions.create('grafie', GrafieExtension)
 
+        project.task('encryptFiles') {
+            println('### Encrypting files.')
+        }
+
+        project.task('decryptFiles') {
+            println('### Decrypting files.')
+        }
     }
+}
+
+
+class GrafieExtension {
+    String fileExtension = 'grafie'
 }
