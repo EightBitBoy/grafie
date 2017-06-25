cc7a82677f2c0c46fe010ba445d960e1447ff600
Stefan Härtel <haertel.dev@gmail.com>
2017-06-25 09:36:29 +0200

    Remove file

--- src/main/groovy/de/eightbitboy/grafie/Grafie.groovy
+++ src/main/groovy/de/eightbitboy/grafie/Grafie.groovy
@@ -1,35 +1,12 @@
 package de.eightbitboy.grafie
 
-import org.gradle.api.Plugin
 import org.gradle.api.Project
-
-/*
-https://docs.gradle.org/3.3/userguide/custom_plugins.html
-https://guides.gradle.org/writing-gradle-plugins/
-https://guides.gradle.org/implementing-gradle-plugins/
-https://github.com/gradle/gradle/tree/master/subprojects
-https://speakerdeck.com/bmuschko/gradle-plugin-best-practices-by-example
- */
+import org.gradle.api.Plugin
 
 class Grafie implements Plugin<Project> {
-    @Override
-    void apply(Project project) {
-        project.extensions.create('grafie', GrafieExtension)
+    final static ID = 'de.eightbitboy.grafie'
 
-        project.task('encryptFiles') {
-            println('### Encrypting files.')
-            println("### Using the file extension ${project.grafie.fileExtension}.")
-        }
-
-        project.task('decryptFiles') {
-            println('### Decrypting files.')
-        }
-
-        project.task('crypto', type: FileCryptoTask)
+    void apply(Project target) {
+        target.task('crypto', type: FileCryptoTask)
     }
-}
-
-
-class GrafieExtension {
-    String fileExtension = 'grafie'
 }
