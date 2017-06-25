6f492ae22d1ce6c5b4824e5832ed6f5024b9a56e
Stefan Härtel <haertel.dev@gmail.com>
2017-06-25 09:40:39 +0200

    Add test class

---
+++ src/test/groovy/de/eightbitboy/grafie/FileCryptoTaskTest.groovy
@@ -0,0 +1,17 @@
+package de.eightbitboy.grafie
+
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import spock.lang.Specification
+
+class FileCryptoTaskTest extends Specification {
+
+    def "add task to project"() {
+        when:
+        Project project = ProjectBuilder.builder().build()
+        def task = project.task('customTask', type: FileCryptoTask)
+
+        then:
+        task instanceof FileCryptoTask
+    }
+}
