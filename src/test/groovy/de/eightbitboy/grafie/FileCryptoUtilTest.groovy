243ba7bcf0bdba0f507d24a1a163b7fcb0d8ea12
Stefan Härtel <haertel.dev@gmail.com>
2017-06-25 20:43:27 +0200

    Add tests

--- src/test/groovy/de/eightbitboy/grafie/FileCryptoUtilTest.groovy
+++ src/test/groovy/de/eightbitboy/grafie/FileCryptoUtilTest.groovy
@@ -1,9 +1,35 @@
 package de.eightbitboy.grafie
 
+import org.gradle.internal.impldep.org.apache.commons.lang.RandomStringUtils
 import spock.lang.Specification
 
 class FileCryptoUtilTest extends Specification {
-    def "handle a password of arbitrary length"() {
+    FileCryptoUtil cryptoUtil
+
+    def setup() {
+        cryptoUtil = new FileCryptoUtil()
+    }
+
+    def "process a password"() {
+        when:
+        byte[] key = cryptoUtil.processKey("secret")
+
+        then:
+        key != null
+        key.length == 16
+    }
+
+    def "process passwords of arbitrary length"() {
+        when:
+        byte[] key = cryptoUtil.processKey(password)
+        then:
+
+        then:
+        key != null
+        key.length == 16
+
+        where:
+        password << (1..200).collect() { RandomStringUtils.random(it) }
     }
 
     def "encrypt a file"() {
