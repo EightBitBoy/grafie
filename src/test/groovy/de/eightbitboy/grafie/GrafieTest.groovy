c62112fd4e903791a991ba9b396c45ba4cfedef2
Stefan Härtel <haertel.dev@gmail.com>
2017-06-25 20:31:39 +0200

    Remove obsolete code

--- src/test/groovy/de/eightbitboy/grafie/GrafieTest.groovy
+++ src/test/groovy/de/eightbitboy/grafie/GrafieTest.groovy
@@ -5,17 +5,6 @@ import org.gradle.testfixtures.ProjectBuilder
 import spock.lang.Specification
 
 class GrafieTest extends Specification {
-    def "run a simple test"() {
-        setup:
-        def numbers = [1, 2]
-
-        when:
-        numbers.add(3)
-
-        then:
-        numbers.size() == 3
-    }
-
     def "add the plugin to a project"() {
         when:
         Project project = ProjectBuilder.builder().build()
