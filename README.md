5614e220d50ae4bd1d8f1532ebff85d88029f957
Stefan Härtel <haertel.dev@gmail.com>
2017-06-30 20:01:43 +0200

    Update the readme

--- README.md
+++ README.md
@@ -6,8 +6,8 @@
 ## Usage
 
 ## Resources that helped developing the plugin
-* [Gradle documentation: Writing Custom Plugins](https://docs.gradle.org/3.3/userguide/custom_plugins.html)
-* [Gradle documentation: The Gradle TestKit](https://docs.gradle.org/3.3/userguide/test_kit.html)
+* [Gradle documentation: Writing Custom Plugins](https://docs.gradle.org/current/userguide/custom_plugins.html)
+* [Gradle documentation: The Gradle TestKit](https://docs.gradle.org/current/userguide/test_kit.html)
 * [Gradle sample: Custom plugin](https://github.com/gradle/gradle/tree/master/subprojects/docs/src/samples/customPlugin)
 * [jonathanhood's gradle-plugin-example](https://github.com/jonathanhood/gradle-plugin-example)
 
--- src/test/groovy/de/eightbitboy/grafie/GrafieTest.groovy
+++ src/test/groovy/de/eightbitboy/grafie/GrafieTest.groovy
@@ -43,8 +43,14 @@ plugins {
     }
 
     /*
-    https://docs.gradle.org/3.3/userguide/test_kit.html
     https://github.com/gradle/gradle/tree/master/subprojects/docs/src/samples/testKit/gradleRunner
+    https://github.com/gradle/gradle/blob/master/design-docs/testing-toolkit.md#milestone-3
+    https://blog.gradle.org/introducing-testkit
+    https://github.com/ysb33r/gradleTest
+    https://github.com/eriwen/gradle-js-plugin/tree/master/src/test/groovy/com/eriwen/gradle/js/util
+    https://discuss.gradle.org/t/separate-execution-for-java-unit-and-integration-tests/8713
+    https://discuss.gradle.org/t/how-do-i-unit-test-custom-tasks/3815
+    https://discuss.gradle.org/t/how-to-execute-a-task-in-unit-test-for-custom-plugin/6771/3
      */
 
     def "add the plugin to a real project"() {
@@ -60,19 +66,7 @@ plugins {
         result.output.contains('decryptFiles')
         result.output.contains('encryptFiles')
     }
-
-    /*
-    https://github.com/gradle/gradle/blob/master/design-docs/testing-toolkit.md#milestone-3
-    https://blog.gradle.org/introducing-testkit
-    https://docs.gradle.org/current/userguide/test_kit.html
-    https://github.com/ysb33r/gradleTest
-    https://github.com/eriwen/gradle-js-plugin/tree/master/src/test/groovy/com/eriwen/gradle/js/util
-    https://discuss.gradle.org/t/separate-execution-for-java-unit-and-integration-tests/8713
-    https://discuss.gradle.org/t/how-do-i-unit-test-custom-tasks/3815
-    https://discuss.gradle.org/t/how-to-execute-a-task-in-unit-test-for-custom-plugin/6771/3
-     */
-
-    @Ignore
+    
     def "encrypt a file"() {
         setup:
         buildFile << """
