7b11f294e32f632f2e7abc7b6b931b70a9f646dc
Stefan Härtel <haertel.dev@gmail.com>
2017-07-12 22:12:37 +0200

    Add function to delete the directory

--- src/test/groovy/de/eightbitboy/grafie/helper/TestProjectDirectory.groovy
+++ src/test/groovy/de/eightbitboy/grafie/helper/TestProjectDirectory.groovy
@@ -1,8 +1,14 @@
 package de.eightbitboy.grafie.helper
 
+import org.apache.commons.io.FileUtils
+
 class TestProjectDirectory extends File {
     public TestProjectDirectory() {
         super([System.getProperty('user.dir'), 'build', 'testProject'].join(separator))
         mkdirs()
+    }
+
+    public void deleteRecursively() {
+        FileUtils.deleteDirectory(this)
     }
 }
