fb7cf17c360ec91184483bd06193286266cb6aaf
Stefan Härtel <haertel.dev@gmail.com>
2017-07-17 20:00:05 +0200

    Add type, formatting

--- src/test/groovy/de/eightbitboy/grafie/testhelper/TestProjectDirectory.groovy
+++ src/test/groovy/de/eightbitboy/grafie/testhelper/TestProjectDirectory.groovy
@@ -2,7 +2,8 @@ package de.eightbitboy.grafie.testhelper
 
 class TestProjectDirectory extends File {
 
-    static final FILE_PATH = [System.getProperty('user.dir'), 'build', 'testProject'].join(separator)
+    static final String FILE_PATH =
+            [System.getProperty('user.dir'), 'build', 'testProject'].join(separator)
 
     TestProjectDirectory() {
         super(FILE_PATH)
