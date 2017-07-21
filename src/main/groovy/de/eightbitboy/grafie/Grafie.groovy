4d48f450d5def59fdf4ad4f64e6dcd67a550d8a8
Stefan Härtel <haertel.dev@gmail.com>
2017-07-21 19:42:37 +0200

    Cleanup, rename constants

--- src/main/groovy/de/eightbitboy/grafie/Grafie.groovy
+++ src/main/groovy/de/eightbitboy/grafie/Grafie.groovy
@@ -5,39 +5,30 @@ import org.gradle.api.Plugin
 
 class Grafie implements Plugin<Project> {
     final static String ID = 'de.eightbitboy.grafie'
-    final static String GROUP = 'Grafie'
-    final static String suffix = '.grafie'
+    final static String TASK_GROUP = 'Grafie'
+    final static String DEFAULT_FILE_SUFFIX = '.grafie'
 
     void apply(Project project) {
         project.extensions.create('grafie', GrafieExtension)
 
         project.task('decryptFiles', type: FileCryptoTask) {
-            group = GROUP
+            group = TASK_GROUP
             description = 'Decrypt all files which have the encryption file name extension, ' +
                     'the default is \'.grafie\'.'
 
             mode = FileCryptoTask.Mode.DECRYPT
             password = project.grafie.password
-            fileSuffix = suffix
+            fileSuffix = DEFAULT_FILE_SUFFIX
         }
 
         project.task('encryptFiles', type: FileCryptoTask) {
-            group = GROUP
+            group = TASK_GROUP
             description = 'Encrypt all files for which a file with the same name ' +
                     'and encryption file name extension exists, the default is \'.grafie\'.'
 
             mode = FileCryptoTask.Mode.ENCRYPT
             password = project.grafie.password
-            fileSuffix = suffix
+            fileSuffix = DEFAULT_FILE_SUFFIX
         }
     }
-
-    /*
-    void validateExtension(Project project) {
-        if (!project.grafie.password) {
-            throw new IllegalStateException(
-                    'No password has been provided for the Grafie plugin!')
-        }
-    }
-    */
 }
