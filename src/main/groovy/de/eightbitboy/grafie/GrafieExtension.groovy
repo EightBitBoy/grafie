1e51f7af3a06d4914049f7da7e087c5daa178b87
Stefan Härtel <haertel.dev@gmail.com>
2017-07-22 22:02:56 +0200

    Add some documentation

--- src/main/groovy/de/eightbitboy/grafie/GrafieExtension.groovy
+++ src/main/groovy/de/eightbitboy/grafie/GrafieExtension.groovy
@@ -1,5 +1,13 @@
 package de.eightbitboy.grafie
 
+/**
+ * GrafieExtension is the extension object accessible in the gradle build file.
+ * It is used for configuring the plugin.
+ */
 class GrafieExtension {
+
+    /**
+     * The password used for encrypting files.
+     */
     String password = ''
 }
