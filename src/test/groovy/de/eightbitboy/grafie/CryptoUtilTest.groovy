e4bccb1d1c3d394d3cd3e2c430f7739dadfc2be8
Stefan Härtel <haertel.dev@gmail.com>
2017-07-27 22:52:08 +0200

    Move tests to new class, add and adjust tests

--- src/test/groovy/de/eightbitboy/grafie/CryptoUtilTest.groovy
+++ src/test/groovy/de/eightbitboy/grafie/CryptoUtilTest.groovy
@@ -12,46 +12,6 @@ class CryptoUtilTest extends Specification {
         cryptoUtil = new FileCryptoUtil('password', '.encrypted')
     }
 
-    def "a password must not be null"() {
-        when:
-        cryptoUtil.processPassword(null)
-
-        then:
-        thrown(IllegalArgumentException)
-    }
-
-    def "a password must not be empty"() {
-        when:
-        cryptoUtil.processPassword('')
-
-        then:
-        thrown(IllegalArgumentException)
-    }
-
-    def "process a password"() {
-        when:
-        byte[] key = cryptoUtil.processPassword('secret')
-
-        then:
-        key != null
-        ArrayUtils.isNotEmpty(key)
-        key.length == 16
-    }
-
-    def "process passwords of arbitrary length"() {
-        when:
-        byte[] key = cryptoUtil.processPassword(password)
-        then:
-
-        then:
-        key != null
-        ArrayUtils.isNotEmpty(key)
-        key.length == 16
-
-        where:
-        password << (1..200).collect() { RandomStringUtils.random(it) }
-    }
-
     def "encrypt a file"() {
         setup:
         File file = new File('file.txt')
--- src/test/groovy/de/eightbitboy/grafie/PasswordConverterTest.groovy
+++ src/test/groovy/de/eightbitboy/grafie/PasswordConverterTest.groovy
@@ -1,6 +1,53 @@
 package de.eightbitboy.grafie
 
+import org.apache.commons.lang3.ArrayUtils
+import org.gradle.internal.impldep.org.apache.commons.lang.RandomStringUtils
 import spock.lang.Specification
 
 class PasswordConverterTest extends Specification {
+    def "a password must not be null"() {
+        when:
+        PasswordConverter.convert(null)
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+    def "a password must not be empty"() {
+        when:
+        PasswordConverter.convert('')
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+    def "a converted password is not null or empty"() {
+        when:
+        byte[] key = PasswordConverter.convert('secret')
+
+        then:
+        key != null
+        ArrayUtils.isNotEmpty(key)
+    }
+
+    def "a converted password has a length of 16 byte"() {
+        when:
+        byte[] key = PasswordConverter.convert('secret')
+
+        then:
+        key.length == 16
+    }
+
+    def "convert passwords of arbitrary length"() {
+        when:
+        byte[] key = PasswordConverter.convert(password)
+
+        then:
+        key != null
+        ArrayUtils.isNotEmpty(key)
+        key.length == 16
+
+        where:
+        password << (1..200).collect() { RandomStringUtils.random(it) }
+    }
 }
