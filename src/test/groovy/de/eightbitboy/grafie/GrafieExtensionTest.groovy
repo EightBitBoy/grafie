301479db42d3f550d8008f85cbdbf5a42ed034cd
Stefan Härtel <haertel.dev@gmail.com>
2017-08-10 10:28:44 +0200

    Implement tests

--- src/test/groovy/de/eightbitboy/grafie/GrafieExtensionTest.groovy
+++ src/test/groovy/de/eightbitboy/grafie/GrafieExtensionTest.groovy
@@ -3,4 +3,23 @@ package de.eightbitboy.grafie
 import spock.lang.Specification
 
 class GrafieExtensionTest extends Specification {
+
+    GrafieExtension extension
+
+    def setup() {
+        extension = new GrafieExtension()
+    }
+
+    def "set and get the password"() {
+        when:
+        extension.setPassword('secret')
+
+        then:
+        extension.getPassword() == 'secret'
+    }
+
+    def "the password is null"() {
+        expect:
+        extension.getPassword() == null
+    }
 }
