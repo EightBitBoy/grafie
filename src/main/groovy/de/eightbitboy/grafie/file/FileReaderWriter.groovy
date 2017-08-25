cdefaa01381e87c1f8430db3b06492199904081f
Stefan Härtel <haertel.dev@gmail.com>
2017-08-25 09:49:47 +0200

    Implement file logic

--- src/main/groovy/de/eightbitboy/grafie/file/FileReaderWriter.groovy
+++ src/main/groovy/de/eightbitboy/grafie/file/FileReaderWriter.groovy
@@ -6,4 +6,24 @@ class FileReaderWriter {
     FileReaderWriter(boolean encodeBase64) {
         this.encodeBase64 = encodeBase64
     }
+
+    void writeBytes(byte[] bytes, File file) {
+        if (encodeBase64) {
+            file.withWriter {
+                it.write(Base64.encoder.encodeToString(bytes))
+            }
+        } else {
+            file.withOutputStream {
+                it.write(bytes)
+            }
+        }
+    }
+
+    byte[] readBytes(File file) {
+        if (encodeBase64) {
+            return Base64.decoder.decode(file.text)
+        } else {
+            return file.readBytes()
+        }
+    }
 }
